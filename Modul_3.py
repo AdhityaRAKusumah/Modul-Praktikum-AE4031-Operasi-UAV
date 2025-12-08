f5a4b5d6cfcbe3c2382384a9dfc31b7b9c95d08f
Adhitya RA Kusumah <147297005+AdhityaRAKusumah@users.noreply.github.com>
2025-12-08 16:51:05 +0700

    Add files via upload
    Uploaded template code for Module 3

---
+++ Modul_3.py
@@ -0,0 +1,66 @@
+# Step 1: Import libraries
+import time, traceback, ReadTelloStream
+from djitellopy import Tello
+
+def FlightControl(command):
+    movement, value = command.split()[0], command.split()[1]
+
+    if movement.lower() == 'forward':
+        drone.move_forward(value)
+    elif movement.lower() == 'back':
+        drone.move_back(value)
+    elif movement.lower() == 'right':
+        drone.move_right(value)
+    elif movement.lower() == 'left':
+        drone.move_left(value)
+    elif movement.lower() == 'ccw':
+        drone.rotate_counter_clockwise(value)
+    elif movement.lower() == 'cw':
+        drone.rotate_clockwise(value)
+    elif movement.lower() == 'up':
+        drone.move_up(value)
+    elif movement.lower() == 'down':
+        drone.move_down(value)
+
+# Step 2: Define flight plan
+FlightPlan = ['forward 20',
+              'cw 90',
+              'up 20',
+              'left 20']
+
+# Step 3: Start
+try:
+    drone = Tello()
+    drone.connect()
+    print('Tello connected!')
+
+    drone.streamon()
+    time.sleep(10)
+
+    drone.takeoff()
+    time.sleep(2)
+
+    for cmd in FlightPlan:
+        img = ReadTelloStream.ReadCamera(drone)
+        FlightControl(cmd)
+        time.sleep(5)
+
+    img = ReadTelloStream.ReadCamera(drone)
+    time.sleep(2)
+
+    ReadTelloStream.SaveImage(img)
+
+    drone.land()
+    time.sleep(2)
+
+    drone.streamoff()
+except:
+    try:
+        drone.land()
+    except:
+        drone.emergency()
+    
+    print('An error occurred!')
+    print('\n' + traceback.format_exc())
+
+drone.end()
